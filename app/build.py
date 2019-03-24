f146a1a2352f008c28ee19648a4716ec4bbaefc3
Radu Potop <radu@wooptoo.com>
2019-03-24 21:33:21 +0000

    Build db, import CSV, docker db image

--- app/build.py
+++ app/build.py
@@ -1,10 +1,21 @@
 from model import db, Ships
-from config import DB_NAME
+import csv
 
 
-def build():
+def build_db():
     db.connect()
     db.create_tables((Ships,))
 
 
-build()
+def import_csv():
+    with open('../positions.csv') as f:
+        reader = csv.reader(f)
+        with db.atomic():
+            for row in reader:
+                Ships.create(
+                    imo=row[0], timestamp=row[1], latitude=row[2], longitude=row[3]
+                )
+
+
+build_db()
+import_csv()
