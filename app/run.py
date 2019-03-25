e90941a87b72c82f8de76cf4c4c294166a064a79
Radu Potop <radu@wooptoo.com>
2019-03-25 09:17:35 +0000

    Fix ships csv

--- app/run.py
+++ app/run.py
@@ -1,10 +1,9 @@
-from flask import Flask, abort, jsonify, Response, logging
-
+from flask import Flask, Response, abort, jsonify, logging
 from webargs import fields
 from webargs.flaskparser import use_args
 
-from model import db, Ships
-from schema import ShipsSchema
+from model import Positions, Ships, db
+from schema import PositionsSchema, ShipsSchema
 
 app = Flask(__name__)
 
@@ -18,10 +17,14 @@ def ships():
 
 @app.route('/api/positions/<imo>')
 def positions(imo):
-    result = Ships.select().where(Ships.imo == imo).order_by(Ships.timestamp.desc())
-    schema = ShipsSchema(many=True)
+    result = (
+        Positions.select()
+        .where(Positions.imo == imo)
+        .order_by(Positions.timestamp.desc())
+    )
+    schema = PositionsSchema(many=True)
     return jsonify({'result': schema.dump(result).data})
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8000)
--- app/schema.py
+++ app/schema.py
@@ -3,6 +3,11 @@ from marshmallow import Schema, fields
 
 class ShipsSchema(Schema):
     imo = fields.Int()
+    name = fields.Str()
+
+
+class PositionsSchema(Schema):
+    imo = fields.Int()
     timestamp = fields.DateTime()
     latitude = fields.Float()
     longitude = fields.Float()
